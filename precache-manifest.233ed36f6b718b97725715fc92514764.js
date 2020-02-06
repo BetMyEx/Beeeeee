bb6bc0a0c2073c117f1ff02c876673fed7fb3d89
BetMyEx <pudovkin1996@gmail.com>
2020-02-06 14:56:39 +0300

    Updates

---
+++ precache-manifest.233ed36f6b718b97725715fc92514764.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e40c29abd0d101aa96f2345c5c139d9",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "d21fdeb87e4f9a07138f",
+    "url": "/my-app/static/css/main.003b3e1c.chunk.css"
+  },
+  {
+    "revision": "286e202e7ea5735ee523",
+    "url": "/my-app/static/js/2.59454491.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/my-app/static/js/2.59454491.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d21fdeb87e4f9a07138f",
+    "url": "/my-app/static/js/main.137d20d0.chunk.js"
+  },
+  {
+    "revision": "5a704d93a1dfd8c8cd26",
+    "url": "/my-app/static/js/runtime-main.4db2cc33.js"
+  }
+]);
