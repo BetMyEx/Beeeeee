cc3298506a57861b8295b0928c8dd2b06d7e92f6
BetMyEx <pudovkin1996@gmail.com>
2020-02-07 14:14:16 +0300

    Updates

---
+++ precache-manifest.423a5141d426860522aee6c23c24e506.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b124f413e288465178f2c1fedccc5244",
+    "url": "/Beeeeee/index.html"
+  },
+  {
+    "revision": "765d51b81370c0cee8bc",
+    "url": "/Beeeeee/static/css/main.ad0cd9b9.chunk.css"
+  },
+  {
+    "revision": "9ea2ec5542ff43ba0f4d",
+    "url": "/Beeeeee/static/js/2.40b03895.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/Beeeeee/static/js/2.40b03895.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "765d51b81370c0cee8bc",
+    "url": "/Beeeeee/static/js/main.81472c69.chunk.js"
+  },
+  {
+    "revision": "8ee7da979f2b9921fce3",
+    "url": "/Beeeeee/static/js/runtime-main.93ac7a74.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Beeeeee/precache-manifest.2c69e9b01d65c8b2ddb3270b5a588e0c.js"
+  "/Beeeeee/precache-manifest.423a5141d426860522aee6c23c24e506.js"
 );
 
 self.addEventListener('message', (event) => {
